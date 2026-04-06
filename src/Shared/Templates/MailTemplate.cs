824be25dcbdf5d8043fd57f4e32e3cf41730552e
Caio Santos <caiosantos.dev@outlook.com>
2026-04-06 16:23:17 -0300

    fix

--- src/Shared/Templates/MailTemplate.cs
+++ src/Shared/Templates/MailTemplate.cs
@@ -29,27 +29,41 @@ namespace api_infor_cell.src.Shared.Templates
         public async Task<string> ConfirmAccount(string name, string code)
         {
             Template? templates = await context.Templates.Find(x => x.Code == "CONFIRM_ACCOUNT" && !x.Deleted).FirstOrDefaultAsync();
-            string html = templates?.Html ?? "";
+            string html = $"{code}";
             
-            html = html.Replace("{{name}}", name).Replace("{{code}}", code);
+            if(templates is not null)
+            {
+                html = html.Replace("{{name}}", name).Replace("{{code}}", code);
+                html = templates.Html;
+            }
 
             return html;
         }
         public async Task<string> NewCodeConfirmAccount(string name, string code)
         {
             Template? templates = await context.Templates.Find(x => x.Code == "NEW_CODE_CONFIRM_ACCOUNT" && !x.Deleted).FirstOrDefaultAsync();
-            string html = templates?.Html ?? "";
+            string html = $"{code}";
             
-            html = html.Replace("{{name}}", name).Replace("{{code}}", code);
+            if(templates is not null)
+            {
+                html = html.Replace("{{name}}", name).Replace("{{code}}", code);
+                html = templates.Html;
+            }
 
             return html;
         }
         public async Task<string> NewLinkCodeConfirmAccount(string name, string code)
         {
             Template? templates = await context.Templates.Find(x => x.Code == "NEW_LINK_CODE_CONFIRM_ACCOUNT" && !x.Deleted).FirstOrDefaultAsync();
-            string html = templates?.Html ?? "";
+
+            string html = $"{code}";
             
-            html = html.Replace("{{name}}", name).Replace("{{code}}", code).Replace("{{ui_uri}}", UiURI);
+            if(templates is not null)
+            {
+                html = templates.Html;
+                html = html.Replace("{{name}}", name).Replace("{{code}}", code).Replace("{{ui_uri}}", UiURI);
+            }
+            
 
             return html;
         }
